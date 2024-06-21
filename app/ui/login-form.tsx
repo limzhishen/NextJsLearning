4f674f1a17c64d84ba5f97b3b09bbd3aacc9d9cf
lzs <zhishenlim96@gmail.com>
2024-06-21 15:26:20 +0800

    login useActionState

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -7,11 +7,11 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
-import {useActionState} from 'react';
+import {useFormState} from 'react-dom';
 import { authenticate } from '../lib/actions';
 
 export default function LoginForm() {
-  const [errorMessage,formAction,isPending]=useActionState(authenticate,undefined);
+  const [errorMessage,formAction,isPending]=useFormState(authenticate,undefined);
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
