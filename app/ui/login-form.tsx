97842b676b289e5c2f1e14f5972695c65c9ca8df
lzs <zhishenlim96@gmail.com>
2024-07-20 00:02:07 +0800

    Retry

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,5 @@
-'use client'
+'use client';
+ 
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -6,15 +7,16 @@ import {
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+import { Button } from '@/app/ui/button';
 import { useActionState } from 'react';
-import { authenticate } from '../lib/actions';
-
-
+import { authenticate } from '@/app/lib/actions';
+ 
 export default function LoginForm() {
-  const [errorMessage,formAction,isPending]=useActionState(
-    authenticate,undefined
-  )
+  const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined,
+  );
+ 
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -65,10 +67,14 @@ export default function LoginForm() {
         <Button className="mt-4 w-full" aria-disabled={isPending}>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
-        <div className="flex h-8 items-end space-x-1">
-          {errorMessage &&(
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {errorMessage && (
             <>
-              <ExclamationCircleIcon className='h-5 w-5 text-red-500'/>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p className="text-sm text-red-500">{errorMessage}</p>
             </>
           )}
--- auth.config.ts
+++ auth.config.ts
@@ -1,23 +1,21 @@
-import { url } from "inspector";
-import type { NextAuthConfig } from "next-auth";
-import { signIn } from "next-auth/react";
-
-export const authConfig={
-    pages:{
-        signIn:'/login'
+import type { NextAuthConfig } from 'next-auth';
+ 
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
     },
-    callbacks:{
-        authorized({auth,request:{nextUrl}}){
-            const isLogin=!!auth?.user
-            const isOnDashboard=nextUrl.pathname.startsWith('/dashboard')
-            if(isOnDashboard){
-                if(isLogin)return true;
-                return false
-            }else if (isLogin){
-                return Response.redirect(new URL('/dashboard',nextUrl))
-            }
-            return true
-        }
-    },
-    providers:[]
-}satisfies NextAuthConfig;
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
