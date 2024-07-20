37ccb2caa682fbb9e224c969162e4463d7a400ef
lzs <zhishenlim96@gmail.com>
2024-07-20 13:07:32 +0800

    Debug 4

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -24,8 +24,7 @@ export default function SideNav() {
           action={async () => {
             'use server';
             await signOut();
-            revalidatePath('login');
-            redirect('/login')
+            
           }}
         >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
